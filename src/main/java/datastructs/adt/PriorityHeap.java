d7fc156ca0bd2981426a16fe70c0f9e421ec5f87
jethronap <efthioa@gmail.com>
2019-11-13 15:26:09 +0200

    added constructor and methods to override

--- src/main/java/datastructs/adt/PriorityHeap.java
+++ src/main/java/datastructs/adt/PriorityHeap.java
@@ -1,4 +1,23 @@
 package datastructs.adt;
 
-public class PriorityHeap {
+import datastructs.adt.utils.ITreeInsertStrategy;
+
+public class PriorityHeap<E> extends Tree<E> {
+
+    /**
+     * Constructor
+     */
+    protected PriorityHeap(ITreeInsertStrategy insertStrategy) {
+        super(insertStrategy);
+    }
+
+    @Override
+    public void push(E element) {
+
+    }
+
+    @Override
+    public boolean contains(E data) {
+        return false;
+    }
 }
