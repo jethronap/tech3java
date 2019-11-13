92139c92b20604ecbbd0f4723586660bce8ccf1c
jethronap <efthioa@gmail.com>
2019-11-13 15:43:07 +0200

    added createRoot method

--- src/main/java/datastructs/adt/PriorityHeap.java
+++ src/main/java/datastructs/adt/PriorityHeap.java
@@ -1,7 +1,12 @@
 package datastructs.adt;
 
 import datastructs.adt.utils.ITreeInsertStrategy;
+import datastructs.adt.utils.TreeNodeCreator;
 
+/**
+ * An implementation of Priority Heap represented as an array.
+ * @param <E>
+ */
 public class PriorityHeap<E> extends Tree<E> {
 
     /**
@@ -15,6 +20,19 @@ public class PriorityHeap<E> extends Tree<E> {
     public void push(E element) {
 
     }
+
+
+    /**
+     * Create the root element
+     * @param element
+     */
+    protected void createRoot(E element){
+
+        TreeNodeCreator<E> creator = new TreeNodeCreator<>();
+        super.root_  = creator.create(element, null, 0, 2);
+        super.nNodes_++;
+    }
+
 
     @Override
     public boolean contains(E data) {
